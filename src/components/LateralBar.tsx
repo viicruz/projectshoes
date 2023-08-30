fda3d6020c7c263f691b136e0a7d34ce7ff47a69
Victor Cruz <VicttorrHugo100@gmail.com>
2023-08-30 14:43:23 -0300

    Update LateralBar.tsx

--- src/components/LateralBar.tsx
+++ src/components/LateralBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from "next/link";
 
 //Libraries Imports
 import * as Accordion from '@radix-ui/react-accordion';
@@ -7,9 +8,6 @@ type Props = {}
 
 export default function LateralBar({ }: Props) {
     return (
-
-
-
         <div className='bg-gray-200 w-80 min-h-screen flex flex-col'>
             <h1 className='font-bold text-4xl justify-center flex mt-2'>Project Shoes</h1>
             <div className='w-full h-full relative flex-1'>
@@ -21,53 +19,53 @@ export default function LateralBar({ }: Props) {
                     <Accordion.Item value="item-1-calçados" className="w-full p-2 border border-black/10 rounded-xl">
                         <Accordion.Header className="items-start justify-start w-full">
                             <Accordion.Trigger className="items-start justify-start w-full">
-                                <h2 className="w-full text-2xl font-bold text-start">Calçados</h2>
+                                <h2 className="w-full text-2xl font-bold ml-3 text-start">Calçados</h2>
                             </Accordion.Trigger>
                         </Accordion.Header>
                         <Accordion.Content className="flex flex-col items-start justify-start bg-gray-300/10 AccordionContent">
+                            <div className='flex flex-col'>
+                                <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg text-start font-bold">
+                                    Visão Geral
+                                </button>
+                                <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg text-start font-bold">
+                                    Cadastrar Calçado
+                                </button>
+                                <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg text-start font-bold">
+                                    Buscar
+                                </button>
+                            </div>
 
-                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg  font-bold">
-                                Visão Geral
-                            </button>
-                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg  font-bold">
-                                Cadastrar Calçado
-                            </button>
-                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg ho font-bold">
-                                Buscar
-                            </button>
                         </Accordion.Content>
                     </Accordion.Item>
 
                     <Accordion.Item value="item-2-vendas" className="w-full p-2 border border-black/10 rounded-xl">
                         <Accordion.Header className="items-start justify-start w-full">
                             <Accordion.Trigger className="items-start justify-start w-full">
-                                <h2 className="w-full text-2xl font-bold text-start">Vendas</h2>
+                                <h2 className="w-full text-2xl font-bold ml-3 text-start">Vendas</h2>
                             </Accordion.Trigger>
                         </Accordion.Header>
                         <Accordion.Content className="flex flex-col items-start justify-start bg-gray-300/10 AccordionContent">
 
-                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg  font-bold">
+                            <div className='flex flex-col'>
+                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg text-start  font-bold">
                                 Dados de vendas
                             </button>
-                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg  font-bold">
+                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg text-start  font-bold">
                                 Gráficos Detalhados
                             </button>
-                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg ho font-bold">
+                            <button className="w-full py-2 px-4 my-1 hover:bg-gray-300 rounded-lg text-start  font-bold">
                                 Percentual Trimestral
                             </button>
+                            </div>
                         </Accordion.Content>
                     </Accordion.Item>
                 </Accordion.Root>
-                <div className='w-full flex absolute bottom-0 left-0 justify-center items-end pb-5'>
+                
+                    <Link className='w-full flex absolute bottom-0 left-0 justify-center items-end pb-5' href="/">
                     <button className='w-[80%] h-8 bg-orange-400 text-white text-center rounded-lg hover:bg-orange-500 font-semibold font-sans'>Ir ao Site</button>
-
-                </div>
-
+                    </Link>
+                
             </div>
-
-
-
-
         </div>
     )
 }
